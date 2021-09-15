babac17e69c6bf8d2d6ad2faa637b6503970fb8b
react-one <dev@reactone.digital>
2021-09-15 16:02:35 +0100

    test ReactOneCustomMethod by JavaScript

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
-import {SafeAreaView, useColorScheme, Text} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  NativeModules,
+  Button,
+  StyleSheet,
+} from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const {ReactOneCustomMethod} = NativeModules;
+  const [id, setId] = useState('Press the button to get The ID');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const getId = () => {
+    ReactOneCustomMethod.getPhoneID()
+      .then((res: string) => {
+        setId('ID: ' + res);
+      })
+      .catch((err: any) => {
+        console.error(err);
+      });
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <Text>My App</Text>
+    <SafeAreaView style={styles.wrapper}>
+      <Text style={styles.id}>{id}</Text>
+      <Button title="Get Id" onPress={getId} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  id: {
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+});
 
 export default App;
