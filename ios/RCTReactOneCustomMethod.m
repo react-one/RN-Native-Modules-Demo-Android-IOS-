d07ded844b920a5f410954484035cf3ec5abbcfb
react-one <dev@reactone.digital>
2021-10-08 18:25:01 +0100

    IOS: cleanup

--- ios/RCTReactOneCustomMethod.m
+++ ios/RCTReactOneCustomMethod.m
@@ -9,17 +9,13 @@
 
 @implementation RCTReactOneCustomMethod
 
-// To export a module named RCTCalendarModule
+// To export a module named ReactOneCustomMethod
 RCT_EXPORT_MODULE();
 
-//RCT_EXPORT_METHOD(getPhoneID)
-//{
-// RCTLogInfo(@"Pretending to create an event");
-//}
 RCT_EXPORT_METHOD(getPhoneID:(RCTPromiseResolveBlock)resolve :(RCTPromiseRejectBlock)reject)
 {
-//  NSUUID *thingToReturn = [[UIDevice currentDevice] identifierForVendor];
-  NSString *thingToReturn = [[UIDevice currentDevice] name];
-  resolve(thingToReturn);
+//  NSUUID *deviceID = [[UIDevice currentDevice] identifierForVendor];
+  NSString *deviceName = [[UIDevice currentDevice] name];
+  resolve(deviceName);
 }
 @end
